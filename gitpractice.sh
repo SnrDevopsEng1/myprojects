ae4bf1c5051f82880807406c568f95dbdb0615c8
SnrDevopsEng1 <seremm254@gmail.com>
2024-08-03 00:10:56 -0400

    new info

---
+++ gitpractice.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# 
+#
+# read -p "What is your name? " NAME
+#
+# read -p "Are you taking the utrains class?" c
+#
+#  
+#
+#  if [ $c = yes ]
+#
+#  then 
+#
+#    echo "Good job $NAME"
+#
+#    else 
+#
+#      echo "Please check the website (utrain.sorg)"
+#
+#      fi
+#
+#      #echo "Your name is $NAME and your answer is $c to the utrains class taking"
+#
+#      If statement
+#      #!/bin/bash
+#
+#       
+#
+#        if [  -f /etc/passwd ]
+#
+#        then
+#
+#         echo exist
+#
+#         else 
+#
+#          echo Missing
+#
+#          fi
+#
+#          if [ -d /tmp/serge ] 
+#
+#           then 
+#
+#              echo exist
+#
+#              else
+#
+#                 mkdir /tmp/serge
+#
+#                 echo new > /tmp/serg/file
+#
+#                 cat /tmp/serge/file
+#
+#                  
+#
+#                  fi
